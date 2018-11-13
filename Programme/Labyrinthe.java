fbf28ad30cb791451331143c188346f8c0b700a2
Jambon3u <antoine.jambonlafay@gmail.com>
2018-11-13 17:34:53 +0100

    Mise à jour Class Labyrinthe

--- Programme/Labyrinthe.java
+++ Programme/Labyrinthe.java
@@ -5,20 +5,20 @@ public class Labyrinthe {
 	private int largeur;
 	int[][] lab;
 
-	public Labyrinthe(int hauteur, int largeur, int dificulte){
-		this.hauteur=hauteur;
-		this.largeur=largeur;
+	public Labyrinthe(int dificulte){
+		this.hauteur=20;
+		this.largeur=20;
 
 		lab = new int[hauteur][largeur];
 
 		for (int i = 0; i < largeur; i++){
 			lab[0][i] = 2;
-			lab[hauteur - 1][i] = 2;
+			lab[hauteur - 1][i] = 2; //2=contour
 		}
 
 		for (int i = 0; i < hauteur; i++){
 			lab[i][0] = 2;
-			lab[i][largeur - 1] = 2;
+			lab[i][largeur - 1] = 2; 
 		}
 		
 		for (int i = 1; i < largeur - 1; i++){
@@ -29,15 +29,13 @@ public class Labyrinthe {
 
 				if (test < (0.5/(0.5/dificulte))/20){
 
-					lab[i][j] = 10; //10 obstacle
+					lab[i][j] = 1; //1=obstacle
 
 				}
 
 			}
 
 		}
-		
-		lab[3][10] = 20; // Monstre
 
 	}
 
@@ -48,9 +46,21 @@ public class Labyrinthe {
 	public int getLargeur() {
 		return largeur;
 	}
+	
+	public int[][] getLab(){
+		return(this.lab);
+	}
 
 	public int getInt(int i,int j) {
 		return(lab[i][j]);
+	}
+	
+	public void initJoueur(int x,int y) {
+		this.lab[x][y]=20;
+	}
+	
+	public void initFantome(int x, int y) {
+		this.lab[x][y]=30;
 	}
 
 
