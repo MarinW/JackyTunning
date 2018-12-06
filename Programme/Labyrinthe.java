224031a260a396c08ef40e22a701955eda640152
Gromegrome <43602223+Gromegrome@users.noreply.github.com>
2018-12-06 18:20:42 +0100

    Update Labyrinthe.java

--- Programme/Labyrinthe.java
+++ Programme/Labyrinthe.java
@@ -1,4 +1,6 @@
 
+package Class;
+
 public class Labyrinthe {
 
 	private int hauteur;
@@ -6,37 +8,13 @@ public class Labyrinthe {
 	int[][] lab;
 
 	public Labyrinthe(int dificulte){
+		
 		this.hauteur=20;
 		this.largeur=20;
 
-		lab = new int[hauteur][largeur];
-
-		for (int i = 0; i < largeur; i++){
-			lab[0][i] = 2;
-			lab[hauteur - 1][i] = 2; //2=contour
-		}
-
-		for (int i = 0; i < hauteur; i++){
-			lab[i][0] = 2;
-			lab[i][largeur - 1] = 2; 
-		}
+		GenerationLabyrintheTexte.Generation(dificulte, hauteur, largeur);
+		lab = LectureLabyrintheTexte.Lecture(hauteur, largeur);	
 		
-		for (int i = 1; i < largeur - 1; i++){
-
-			for (int j = 1; j < largeur - 1; j++){
-
-				double test = Math.random();
-
-				if (test < (0.5/(0.5/dificulte))/20){
-
-					lab[i][j] = 1; //1=obstacle
-
-				}
-
-			}
-
-		}
-
 	}
 
 	public int getHauteur() {
@@ -46,7 +24,7 @@ public class Labyrinthe {
 	public int getLargeur() {
 		return largeur;
 	}
-	
+
 	public int[][] getLab(){
 		return(this.lab);
 	}
@@ -54,13 +32,18 @@ public class Labyrinthe {
 	public int getInt(int i,int j) {
 		return(lab[i][j]);
 	}
-	
+
 	public void initJoueur(int x,int y) {
 		this.lab[x][y]=20;
 	}
-	
-	public void initFantome(int x, int y) {
-		this.lab[x][y]=30;
+
+	public void initFantome(Fantome[] f) {
+
+		for(Fantome fan:f) {
+			this.lab[fan.getX()][fan.getY()]=30;
+		}
+
+
 	}
 
 	public void initData(int x, int y) {
